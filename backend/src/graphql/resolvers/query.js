93b2fa73ede9120d402685bb5f169dbbd3115ceb
danielvazquezguerra <daniel.vazquezguerra@gmail.com>
2020-08-29 15:05:47 +0200

    mutation query ok

---
+++ backend/src/graphql/resolvers/query.js
@@ -0,0 +1,15 @@
+import Message from '../../models/Message';
+
+const Query = {
+
+    ping() {
+        return "Pong!"
+    },
+
+    messages: async () => {
+      return await Message.find()
+    }
+    
+}
+
+export default Query;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,11 @@
-import { GraphQlServer } from "graphql-yoga";
-import path from 'path';
+
+import { GraphQLServer } from "graphql-yoga";
 import resolvers from './graphql/resolvers';
+import path from 'path';
 
 
 
-export const server  = new GraphQlServer({
+export const server  = new GraphQLServer({
     typeDefs: path.join(__dirname, 'graphql/schema.graphql'), 
     resolvers
 
